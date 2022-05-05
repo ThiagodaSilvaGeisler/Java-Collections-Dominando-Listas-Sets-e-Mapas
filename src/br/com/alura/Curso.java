1be443b08b79037a87962e5968d026ea89d73935
Thiago da Silva Geisler <contato.thiagogeisler@gmail.com>
2022-05-05 02:26:54 -0300

    Usando o método Collections.unmodifiableList(aulas) no método getAulas da classe Curso

--- src/br/com/alura/Curso.java
+++ src/br/com/alura/Curso.java
@@ -1,6 +1,7 @@
 package br.com.alura;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Curso {
@@ -24,7 +25,7 @@ public class Curso {
 	}
 
 	public List<Aula> getAulas() {
-		return aulas;
+		return Collections.unmodifiableList(aulas); // Devolve lista apenas de leitura
 	}
 	
 	public void adiciona(Aula aula) {
