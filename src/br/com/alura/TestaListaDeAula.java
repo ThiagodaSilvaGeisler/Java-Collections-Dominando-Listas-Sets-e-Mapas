5b26311becbca87786a69f69195280561e958d20
Thiago da Silva Geisler <contato.thiagogeisler@gmail.com>
2022-05-05 00:51:54 -0300

    Usando o método Comparator.compararing dentro de Collections.sort

--- src/br/com/alura/TestaListaDeAula.java
+++ src/br/com/alura/TestaListaDeAula.java
@@ -1,12 +1,14 @@
 package br.com.alura;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class TestaListaDeAula {
 	
 	public static void main(String[] args) {
-		Aula a1 = new Aula("Rvistando as ArrayLists", 21);
-		Aula a2 = new Aula("Lista de objetos", 15);
+		Aula a1 = new Aula("Revistando as ArrayLists", 21);
+		Aula a2 = new Aula("Lista de objetos", 20);
 		Aula a3 = new Aula("Relacionamento de listas e objetos", 15);
 		
 		
@@ -16,5 +18,13 @@ public class TestaListaDeAula {
 		aulas.add(a3);
 		
 		System.out.println(aulas);
+		
+		Collections.sort(aulas);
+		
+		System.out.println(aulas);
+		
+		Collections.sort(aulas, Comparator.comparing(Aula::getTempo));
+		
+		System.out.println();
 	}
 }
