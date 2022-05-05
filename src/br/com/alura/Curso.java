57995d13a5d40f52255b6f555a8e90a7b4199cbc
Thiago da Silva Geisler <contato.thiagogeisler@gmail.com>
2022-05-05 20:43:27 -0300

    Criando o conjunto Set de alunos na Classe Curso

--- src/br/com/alura/Curso.java
+++ src/br/com/alura/Curso.java
@@ -2,14 +2,17 @@ package br.com.alura;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Curso {
 
 	private String nome;
 	private String instrutor;
 	private List<Aula> aulas = new ArrayList<Aula>();
-
+	private Set<Aluno> alunos = new HashSet<>();
+	
 	public Curso(String nome, String instrutor) {
 		this.nome = nome;
 		this.instrutor = instrutor;
