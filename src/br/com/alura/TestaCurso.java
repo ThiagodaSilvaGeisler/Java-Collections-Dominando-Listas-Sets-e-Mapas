e548b23c90359dc78f8317710b06f64bd35f6abc
Thiago da Silva Geisler <contato.thiagogeisler@gmail.com>
2022-05-05 02:10:38 -0300

    Brincando com as referências ao mesmo objeto

--- src/br/com/alura/TestaCurso.java
+++ src/br/com/alura/TestaCurso.java
@@ -1,12 +1,20 @@
 package br.com.alura;
 
-import java.util.ArrayList;
+import java.util.List;
 
 public class TestaCurso {
 	public static void main(String[] args) {
-		
+
 		Curso javaColecoes = new Curso("Dominando as coleções do java", "Paulo Silveira");
-		
+
 		List<Aula> aulas = javaColecoes.getAulas();
+		System.out.println(aulas);
+
+		aulas.add(new Aula("Trabalhando com ArrayLista", 21));
+
+		System.out.println(aulas);
+		System.out.println(javaColecoes.getAulas());
+		System.out.println(aulas == javaColecoes.getAulas());
 	}
 }
+		
