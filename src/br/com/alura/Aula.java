ae41f6f1e7a35bf1370f4a2622f7dd74bc8751fa
Thiago da Silva Geisler <contato.thiagogeisler@gmail.com>
2022-05-05 00:25:04 -0300

    Criando Aula e TestaListaDeAula

---
+++ src/br/com/alura/Aula.java
@@ -0,0 +1,21 @@
+package br.com.alura;
+
+public class Aula {
+	
+	private String titutlo;
+	private int tempo;
+	
+	public Aula(String titutlo, int tempo) {
+		this.titutlo = titutlo;
+		this.tempo = tempo;
+	}
+	
+	public String getTitutlo() {
+		return titutlo;
+	}
+	
+	public int getTempo() {
+		return tempo;
+	}
+	
+}
