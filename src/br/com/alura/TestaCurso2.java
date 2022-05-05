b9cf5eaac49be949fc7bf795192ff53ac36c94ab
Thiago da Silva Geisler <contato.thiagogeisler@gmail.com>
2022-05-05 05:08:25 -0300

    Implementando o método toString() em Curso, para usá-lo em System.out.println().

--- src/br/com/alura/TestaCurso2.java
+++ src/br/com/alura/TestaCurso2.java
@@ -22,6 +22,8 @@ public class TestaCurso2 {
 		Collections.sort(aulas);
 		System.out.println(aulas);
 		System.out.println(javaColecoes.getTempoTotal());
+		System.out.println(javaColecoes);
+		
 	}
 }
 		
