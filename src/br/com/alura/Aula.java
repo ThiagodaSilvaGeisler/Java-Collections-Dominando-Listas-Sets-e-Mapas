05e291c91978c9c53e59a27aad7dc33ad42401b7
Thiago da Silva Geisler <contato.thiagogeisler@gmail.com>
2022-05-05 00:34:54 -0300

    Reescrevendo o método toString e passando a lista para o método System.out.println()

--- src/br/com/alura/Aula.java
+++ src/br/com/alura/Aula.java
@@ -2,20 +2,25 @@ package br.com.alura;
 
 public class Aula {
 	
-	private String titutlo;
+	private String titulo;
 	private int tempo;
 	
 	public Aula(String titutlo, int tempo) {
-		this.titutlo = titutlo;
+		this.titulo = titutlo;
 		this.tempo = tempo;
 	}
 	
 	public String getTitutlo() {
-		return titutlo;
+		return titulo;
 	}
 	
 	public int getTempo() {
 		return tempo;
+	}
+	
+	@Override
+	public String toString() {
+		return "[Aula: " + this.titulo + ", " + this.tempo + " minutos]";
 	}
 	
 }
