a78f563ea1dbab6db4964d288be27bf35b979b51
Thiago da Silva Geisler <contato.thiagogeisler@gmail.com>
2022-05-06 01:00:38 -0300

    TreeSet

--- src/br/com/alura/Curso.java
+++ src/br/com/alura/Curso.java
@@ -2,17 +2,19 @@ package br.com.alura;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class Curso {
 
 	private String nome;
 	private String instrutor;
 	private List<Aula> aulas = new ArrayList<Aula>();
-	//LinkedHashSet guarda a ordem
-	private Set<Aluno> alunos = new LinkedHashSet<>();
+	
+	//TreeSet só funciona para classes que são Comparable, vai usar estrtura de dados árvore rubro negra
+	//Tem busca rápida, mais do que as listas, não tão rápidas quanto HashSet
+	private Set<Aluno> alunos = new TreeSet<>();
 	
 	public Curso(String nome, String instrutor) {
 		this.nome = nome;
